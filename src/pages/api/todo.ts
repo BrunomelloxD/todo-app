21d7c5c71b086dcd55034a249cb28d27f0925152
Bruno <brunomellogithub@gmail.com>
2023-03-14 13:14:52 -0300

    Initial commit

---
+++ src/pages/api/todo.ts
@@ -0,0 +1,14 @@
+import { createTodo } from '@/lib/db'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    if (req.method === 'POST') {
+        const data = JSON.parse(req.body)
+        await createTodo(data)
+        return res.status(200).json({ message: 'Success' })
+    }
+    res.status(200).json({ name: 'John Doe' })
+}
